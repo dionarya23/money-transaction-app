b8eb575d125dd39070c87e85d0ff31ce0fb40994
Dion Arya Pamungkas <dionarya.p@gmail.com>
2023-09-11 21:37:06 +0700

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Dion Arya Pamungkas on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
